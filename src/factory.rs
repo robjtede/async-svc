5a09ea03b058f250cb0a74127c6f775e132f5927
Rob Ede <robjtede@icloud.com>
2020-11-19 00:47:53 +0000

    bump version

--- src/factory.rs
+++ src/factory.rs
@@ -1,3 +1,5 @@
+use std::convert::Infallible;
+
 use crate::Svc;
 
 pub trait SvcFactory<Req> {
@@ -5,5 +7,18 @@ pub trait SvcFactory<Req> {
     type InitErr;
     type Svc: Svc<Req>;
 
-    fn init_service(&self, cfg: Self::Cfg) -> Result<Self::Svc, Self::InitErr>;
+    fn init_svc(&self, cfg: Self::Cfg) -> Result<Self::Svc, Self::InitErr>;
+}
+
+impl<S, Req> SvcFactory<Req> for S
+where
+    S: Svc<Req> + Clone,
+{
+    type Cfg = ();
+    type InitErr = Infallible;
+    type Svc = S;
+
+    fn init_svc(&self, _: Self::Cfg) -> Result<Self::Svc, Self::InitErr> {
+        Ok(self.clone())
+    }
 }
